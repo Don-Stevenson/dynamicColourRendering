36216a574aa05706ca24aecfb80690331e70f95a
Don-Stevenson <dsteven1@gmail.com>
2020-07-03 17:38:29 -0400

    rendering the route

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
+const sass = require("node-sass-middleware");
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
   });
+
+app.use("/dynamicColours");
---
+++ routes/dynamicColours.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const sass = require("node-sass-middleware");
+
+router.post("/move", (request, response) => {
+  // eslint-disable-next-line camelcase
+});
