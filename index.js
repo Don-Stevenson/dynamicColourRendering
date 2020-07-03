581a0a972285b9b3b53a7c221d5c47428b73c867
Don-Stevenson <dsteven1@gmail.com>
2020-07-03 17:11:48 -0400

    add dotenv

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-// Web server config9
+// load .env data into process.env
+require('dotenv').config();
+
+
+// Web server config
 const PORT = process.env.PORT || 8080;
 const express = require("express");
